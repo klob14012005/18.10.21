537c788c2f3076d760e001a0178294ab1ec73fec
Lobachev Konstantin <klob14012005@gmail.com>
2021-10-18 19:18:26 +0300

    ТестИзменение

--- 18.10.21/MainWindow.xaml.cs
+++ 18.10.21/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace _18._10._21
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
